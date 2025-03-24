2cc3be2285aa87184a3bb8b1e97491b3e1636956
SSujjall <suzallmhz@gmail.com>
2025-03-24 16:05:42 +0545

    Environment Variable test

--- UrlShortner.Server/Program.cs
+++ UrlShortner.Server/Program.cs
@@ -11,7 +11,7 @@ using UrlShortner.Server.Middlewares;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
